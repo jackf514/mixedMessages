dc2bec92a3088961c58ca96a7f545551dd7a8578
jackf514 <jackferguson514@gmail.com>
2022-11-09 10:31:26 -0600

    store message parts in arrays

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // Store message parts in arrays
+const exortAdj = ['exhort', 'urge', 'bid', 'entreat', 'insist', 'beseech', 'advise', 'incite', 'counsel', 'invite', 'request'];
+const bodyAdj = ['fat', 'greasy', 'large', 'abnormal', 'pointy', 'disgusting', 'bulbus', 'flabby', 'nosey', 'strange'];
+const bodyParts = ['nose', 'head', 'foot', 'hair', 'belly', 'hand', 'eyes', 'chin', 'knees', 'elbows'];
+
+// Construct message
