8b48a3b9f70cc5b27293ac5878de8b78a2f9b172
jackf514 <jackferguson514@gmail.com>
2022-11-09 11:09:26 -0600

    create functions needed to create message

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // Store message parts in arrays
-const exortAdj = ['exhort', 'urge', 'bid', 'entreat', 'insist', 'beseech', 'advise', 'incite', 'counsel', 'invite', 'request'];
+const exhortAdj = ['exhort', 'urge', 'bid', 'entreat', 'insist', 'beseech', 'advise', 'incite', 'counsel', 'invite', 'request'];
 const bodyAdj = ['fat', 'greasy', 'large', 'abnormal', 'pointy', 'disgusting', 'bulbus', 'flabby', 'nosey', 'strange'];
 const bodyParts = ['nose', 'head', 'foot', 'hair', 'belly', 'hand', 'eyes', 'chin', 'knees', 'elbows'];
 
 // Construct message
+function chooseParts() { //choose random message parts from each array
+    const parts = [];
+    parts.push(exhortAdj[Math.floor(Math.random() * exhortAdj.length)]);
+    parts.push(bodyAdj[Math.floor(Math.random() * bodyAdj.length)]);
+    parts.push(bodyParts[Math.floor(Math.random() * bodyParts.length)]);
+    return parts;
+};
+
+function createMessage(exhortAdj, bodyAdj, bodyParts) { //put together string from random message parts
+    return `We ${exhortAdj} you to keep your ${bodyAdj} ${bodyParts} out of other people's business`;
+};
+
